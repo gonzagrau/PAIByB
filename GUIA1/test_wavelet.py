e1b288f9cbeca818dedfebf918ba02eac49e7c13
Ibajlec <ibajlec@itba.edu.ar>
2024-08-29 12:21:34 -0300

    wavelets_plotaer cada ruido de nivel

--- GUIA1/test_wavelet.py
+++ GUIA1/test_wavelet.py
@@ -84,34 +84,46 @@ def Wavelet2(image_path:str,
         cv2.destroyAllWindows()
     return reconstructed_image,coeffs
 
-def read_coef(Coeffs:tuple,
-              level:int,plot:bool = True,
-              wavelet:str = 'haar', 
-              mode:str= 'periodization'):
-    
-    tupple_c = Coeffs[-level]
-    recon = pywt.waverec2(tupple_c,wavelet,mode)
-    recon = np.uint8(recon)
-    cv2.imshow('rec', recon)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
+import cv2
+import numpy as np
+import pywt
 
-    
-    return tupple_c
+def read_coef(Coeffs: tuple, 
+              level: int, 
+              plot: bool = True, 
+              wavelet: str = 'haar', 
+              mode: str = 'periodization',
+              coef_a_mostrar_vhd:str = 'v'):
+
+    #vector vacio
+    modified_coeffs = [np.zeros_like(c) for c in Coeffs]
+
+    if level <= len(Coeffs) - 1:
+        cH, cV, cD = Coeffs[-level]
+        if coef_a_mostrar_vhd == 'v':
+            modified_coeffs[-level] = (np.zeros_like(cH), cV, np.zeros_like(cD))
+        elif coef_a_mostrar_vhd == 'h':           
+            modified_coeffs[-level] = (cH, np.zeros_like(np.zeros_like(cV)), np.zeros_like(cD))
+        else:           
+            modified_coeffs[-level] = (np.zeros_like(cH), np.zeros_like(np.zeros_like(cV)),cD)
+
+    else:
+        raise ValueError(f"nivel invalido: {level} o coeficiente {coef_a_mostrar_vhd}.")
+
+    recon = pywt.waverec2(modified_coeffs, wavelet, mode)
+    recon = np.uint8(np.clip(recon, 0, 255))
+
+    if plot:
+        cv2.imshow('Reconstructed Image from Vertical Coefficients', recon)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 
 def main():
-    a, b =Wavelet2(image_path =r'C:\Users\Usuario\Desktop\PIByB\PAIByB\GUIA1\PAIByB-2\Pie2-1.tif',Level= 1)
+    a, b =Wavelet2(image_path =r'PAIByB-2\Pie2-1.tif',Level= 1,Plot_all_levels=False,Plot_reconstructed=False)
     #b =Wavelet(image_path =r'GUIA1\PAIByB-2\Pie2-1.tif')
-    image = cv2.imread(r'C:\Users\Usuario\Desktop\PIByB\PAIByB\GUIA1\PAIByB-2\Pie2-1.tif', cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('Reconstructed Image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    read_coef(b,level=1,plot=True,coef_a_mostrar_vhd='v')
     
-
-
-
 if __name__ == '__main__':
     main()
